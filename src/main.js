7edb755bfb9332a20e561cd8e8a1535bcf9193c8
Erik Irgens <erik.t.irgens@gmail.com>
2020-07-06 08:25:46 -0700

    Update
    Update

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { PokemonService } from './pokemon.js';
+import { PokemonService } from './pokemon-service.js';
 
 $(document).ready(function () {
   $('#findPokemon').click(function () {
@@ -10,18 +10,26 @@ $(document).ready(function () {
 
 
     (async () => {
+      console.log("async iffe")
       let pokeService = new PokemonService();
       const response = await pokeService.getPokemonByName(name);
       getElements(response);
-    });
+      console.log(response)
+    })();
 
 
     const getElements = function (response) {
-      $('.showName').text(`Say hello to ${response.species.name}!`);
-      $('.showSprite').html(`<img src=${response.sprites.front_default}></img>`);
-      $('.showHeight').text(`${response.species.name} is ${response.height} units tall!`);
-      $('.showType').text(`${response.species.name} is ${response.types[0].type.name} type!`);
-      $('.showFirstAbility').html(`${response.species.name} knows the move ${response.abilities[0].ability.name}`);
+      if (response) {
+        console.log(response)
+        $('.showName').text(`Say hello to ${response.species.name}!`);
+        $('.showSprite').html(`<img src=${response.sprites.front_default}></img>`);
+        $('.showHeight').text(`${response.species.name} is ${response.height} units tall!`);
+        $('.showType').text(`${response.species.name} is ${response.types[0].type.name} type!`);
+        $('.showFirstAbility').html(`${response.species.name} knows the move ${response.abilities[0].ability.name}`);
+      } else {
+        $('.showName').text(`Error!`);
+      }
+
     };
   });
 });
--- src/pokemon-service.js
+++ src/pokemon-service.js
@@ -1,6 +1,7 @@
 export class PokemonService {
   async getPokemonByName(name) {
     try {
+      console.log("Trying")
       let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
       let jsonifiedResponse;
       if (response.ok && response.status == 200) {
